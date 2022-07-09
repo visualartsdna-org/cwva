5b7992fe56432728b6660c01c2385d30b4c21d68
rspates <rspates@users.noreply.github.com>
2022-07-09 11:39:54 -0400

    refinements and additions

--- serverCwva2.bat
+++ serverCwva2.bat
@@ -1,4 +1,5 @@
 rem 
-java -cp bin\cwvaServer-2.0.4.jar;bin\nals-1.0.0.jar cwva.Main -ok stuff
+rem java -cp bin\cwvaServer-2.0.4.jar;bin\nals-1.0.0.jar cwva.Main -ok stuff
+java -cp bin\cwvaServer-2.0.4.jar cwva.Main -ok stuff
 
 
