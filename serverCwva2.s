796966e6422ffc7d474c5623ba915ed5c97d3b99
rspates <rspates@users.noreply.github.com>
2022-07-09 11:37:59 -0400

    created new terms

--- serverCwva2.s
+++ serverCwva2.s
@@ -3,5 +3,5 @@
 mv err.log log/err_$(date -d "today" +"%Y%m%d%H%M").log
 mv out.log log/out_$(date -d "today" +"%Y%m%d%H%M").log
 #
-sudo java -cp bin/cwvaServer-2.0.4.jar:bin/lsys-2.0.2.jar cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
+sudo java -cp bin/cwvaServer-2.0.4.jar cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
 
