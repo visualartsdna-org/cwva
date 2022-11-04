464a68a5e5ce2a87f1beff6244fdc0be18a296cf
rspates <rspates@users.noreply.github.com>
2022-11-04 08:51:52 -0400

    updated jar version

--- serverCwva2.s
+++ serverCwva2.s
@@ -3,5 +3,5 @@
 mv err.log log/err_$(date -d "today" +"%Y%m%d%H%M").log
 mv out.log log/out_$(date -d "today" +"%Y%m%d%H%M").log
 #
-sudo java -cp bin/cwvaServer-2.0.4.jar cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
+sudo java -cp bin/cwvaServer-2.1.0.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170 cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
 
--- serverFcn.s
+++ serverFcn.s
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
-mv fcn/err.log fcn/log/err_$(date -d "today" +"%Y%m%d%H%M").log
-mv fcn/out.log fcn/log/out_$(date -d "today" +"%Y%m%d%H%M").log
+mv ferr.log log/ferr_$(date -d "today" +"%Y%m%d%H%M").log
+mv fout.log log/fout_$(date -d "today" +"%Y%m%d%H%M").log
 #
-sudo java -cp bin/cwvaServer-2.0.3.jar:bin/lsys-2.0.1.jar function.Main -cfg serverFcn.rson >fcn/out.log 2>fcn/err.log &
+# Why does this one have lsys in path?
+#sudo java -cp bin/cwvaServer-2.1.0.jar:bin/lsys-2.0.1.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170 function.Main -cfg serverFcn.rson >fcn/out.log 2>fcn/err.log &
+sudo java -cp bin/cwvaServer-2.1.0.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170 function.Main -cfg serverFcn.rson >fout.log 2>ferr.log &
 
