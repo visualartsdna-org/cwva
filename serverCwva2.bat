e546b1caec44d6997b37cefb388d77a410758416
rspates <rspates@users.noreply.github.com>
2022-11-05 11:18:34 -0400

    updated startups

--- serverCwva2.bat
+++ serverCwva2.bat
@@ -1,5 +1,5 @@
 rem 
 rem java -cp bin\cwvaServer-2.0.4.jar;bin\nals-1.0.0.jar cwva.Main -ok stuff
-java -cp bin\cwvaServer-2.0.4.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170  cwva.Main -ok stuff
+java -cp bin\cwvaServer-2.0.4.jar -Dgcp_bucket=?  cwva.Main -ok stuff
 
 
--- serverFcn.s
+++ serverFcn.s
@@ -5,5 +5,5 @@ mv fout.log log/fout_$(date -d "today" +"%Y%m%d%H%M").log
 #
 # Why does this one have lsys in path?
 #sudo java -cp bin/cwvaServer-2.1.0.jar:bin/lsys-2.0.1.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170 function.Main -cfg serverFcn.rson >fcn/out.log 2>fcn/err.log &
-sudo java -cp bin/cwvaServer-2.1.0.jar -Dgcp_bucket=cloud-ai-platform-b42a042e-a998-4243-a202-e1c90e536170 function.Main -cfg serverFcn.rson >fout.log 2>ferr.log &
+sudo java -cp bin/cwvaServer-2.1.0.jar -Dgcp_bucket=? function.Main -cfg serverFcn.rson >fout.log 2>ferr.log &
 
