d1e350455a9e20482181d9e01fc912a513a2894d
rspates <rspates@users.noreply.github.com>
2022-11-07 10:11:36 -0500

    changed server jar location

--- serverCwva2.s
+++ serverCwva2.s
@@ -3,5 +3,5 @@
 mv err.log log/err_$(date -d "today" +"%Y%m%d%H%M").log
 mv out.log log/out_$(date -d "today" +"%Y%m%d%H%M").log
 #
-sudo java -cp bin/cwvaServer-2.1.0.jar -Dgcp_bucket=$gcp_bucket cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
+sudo java -cp ~/cwvaServer/target/cwvaServer-2.1.0.jar -Dgcp_bucket=$gcp_bucket cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
 
