e837b601d76b1e27fe121e9a18d5befadfab1270
rspates <rspates@users.noreply.github.com>
2023-10-31 12:59:55 -0400

    Updated from recent server changes

--- serverCwva2.s
+++ serverCwva2.s
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
+cd /home/rick_spates/cwva
+#
 mv err.log log/err_$(date -d "today" +"%Y%m%d%H%M").log
 mv out.log log/out_$(date -d "today" +"%Y%m%d%H%M").log
 #
-sudo java -cp ~/cwvaServer/target/cwvaServer-2.1.0.jar -Dgcp_bucket=$gcp_bucket cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
+export PATH=$PATH:~/bin
+#
+sudo java -cp resources:/home/rick_spates/cwvaServer/target/cwvaServer-2.2.0.jar -Dgcp_bucket=$gcp_bucket cwva.Main -cfg serverCwva2.rson >out.log 2>err.log &
 
